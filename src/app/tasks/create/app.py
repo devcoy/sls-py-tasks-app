977efeb591734dbc3f2e9c88f11ed76295f20aa0
Jorge Cervantes <jcdev1825@gmail.com>
2024-01-18 14:25:36 -0600

    feat(tasks): add create, get, update and delete functions

---
+++ src/app/tasks/create/app.py
@@ -0,0 +1,16 @@
+import json
+import logging
+
+
+def lambda_handler(event, context):
+    try:
+        logging.info(f"event: {event}")
+        data = {
+            "data": None,
+            "message": "Hello World from CREATE tasks lambda function!",
+        }
+
+        return {"statusCode": 200, "body": json.dumps(data)}
+    except Exception as e:
+        logging.error(format(e))
+        return {"statusCode": 500, "body": {"message": json.dumps(e)}}
